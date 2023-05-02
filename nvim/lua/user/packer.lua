98d2b909340448a1727cf625ff00f3ab6d8a83ae
Gregor Wagner <gregamer98@gmail.com>
2023-05-02 09:04:21 +0200

    changes

--- nvim/lua/user/packer.lua
+++ nvim/lua/user/packer.lua
@@ -114,23 +114,44 @@ return require('packer').startup(function(use)
     --=======================================================================
     --==============             LSP                  =======================
     --=======================================================================
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
-            'williamboman/mason.nvim',
-            run = function()
-                pcall(vim.cmd, 'MasonUpdate')
-            end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
-    }
-}end)
+    -- Auto-install lazy.nvim if not present
+    if not vim.loop.fs_stat(lazypath) then
+        print('Installing lazy.nvim....')
+        vim.fn.system({
+            'git',
+            'clone',
+            '--filter=blob:none',
+            'https://github.com/folke/lazy.nvim.git',
+            '--branch=stable', -- latest stable release
+            lazypath,
+        })
+    end
+
+    vim.opt.rtp:prepend(lazypath)
+
+    require('lazy').setup({
+        {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v2.x',
+            dependencies = {
+                -- LSP Support
+                {'neovim/nvim-lspconfig'},             -- Required
+                {                                      -- Optional
+                    'williamboman/mason.nvim',
+                    build = function()
+                        pcall(vim.cmd, 'MasonUpdate')
+                    end,
+                },
+                {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+                -- Autocompletion
+                {'hrsh7th/nvim-cmp'},     -- Required
+                {'hrsh7th/cmp-nvim-lsp'}, -- Required
+                {'L3MON4D3/LuaSnip'},     -- Required
+            }
+        }
+    })
+
+end)
--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -16,13 +16,27 @@ lsp.ensure_installed({
     'eslint',
     'sumneko_lua',
     'rust_analyzer',
+    'php',
+    'python',
+    'python3',
 })
 
-local cmp = require ('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({select=true}),
-    ['<C-Space>'] = cmp.mapping.complete()
+
+-- Make sure you setup `cmp` after lsp-zero
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }
 })
